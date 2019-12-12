36b16b4612d2c665ab87e0a824efe171d8fad5f1
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-11 23:57:40 -0500

    Adds search component to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
