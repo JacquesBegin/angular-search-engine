601616f1441edb535c366ee4029672d392d09243
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-12 00:06:37 -0500

    Adds topbar to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { TopBarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
