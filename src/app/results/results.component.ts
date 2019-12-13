567ae676078aee75c4a7afadabcd0f9bfa1daa09
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-12 23:23:13 -0500

    Creates results component.

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
+})
+export class ResultsComponent {
+
+}
