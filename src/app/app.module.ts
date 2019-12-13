3c8638483956ae151c76ac2ced658243d399dd04
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-12 23:22:42 -0500

    Adds results component to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { TopBarComponent } from './topbar/topbar.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    TopBarComponent
+    TopBarComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
